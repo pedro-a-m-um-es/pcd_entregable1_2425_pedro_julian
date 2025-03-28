f5067a9823e35faa96d91c5b0917b928b0db235b
pedro-a-m-um-es <pedro.a.m@um.es>
2025-03-28 21:16:18 +0100

    Añadiendo y modificando codigo

--- entregable_implementacion_poo.py
+++ entregable_implementacion_poo.py
@@ -1,5 +1,14 @@
+
 from abc import ABCMeta, abstractmethod
 from enum import Enum
+
+
+class ListaDestinos(Enum):
+    LORCA = "LORCA"
+    CARTAGENA = "CARTAGENA"
+    MURCIA = "MURCIA"
+    YECLA = "YECLA"
+    JUMILLA = "JUMILLA"
 
 
 class CanalEntrada(Enum):
@@ -13,118 +22,152 @@ class ModalidadEnvio(Enum):
     CENTRAL = 1
 
 
-class ListaDestinos(Enum):
-    LORCA = "LORCA"
-    CARTAGENA = "CARTAGENA"
-    MURCIA = "MURCIA"
-    YECLA = "YECLA"
-    JUMILLA = "JUMILLA"
-
-
 class Persona(metaclass=ABCMeta):
-    def __init__(self, dni: str, edad: int, nombre: str, direccion: str):
-        self.dni = dni
-        self.edad = edad
+
+    def __init__(self, nombre, direccion, dni):
         self.nombre = nombre
         self.direccion = direccion
+        self.dni = dni
 
     @abstractmethod
-    def get_persona(self):
+    def devolverDatos(self):
         pass
 
 
-class Trabajador(Persona):
-    def __init__(self, dni: str, edad: int, nombre: str, direccion: str, disponible: bool, salario: float):
-        Persona.__init__(self, dni, edad, nombre, direccion)
-        self.disponible = disponible
-        self.salario = salario
+class Paquete:
 
-    def cambiar_disponibilidad(self, disponible: bool):
-        self.disponible = disponible
-    '''
-    def AsignarViaje(self, viaje: 'Viaje'):
-        pass
-    def CerrarParte(self, parte: 'Parte'):
-        pass
-    '''
-    @abstractmethod
-    def get_trabajador(self):
-        pass
+    def __init__(self, peso, destino):
+        self.peso = peso
+        self.destino = destino
+        self.id = None
+        self.entregado = False
+        self.asignado = False
+
+    def devolverDatos(self):
+        print('Paquete:', self.id, ', Peso:', self.peso, ', Destino:', self.destino,
+              ', Entregado:', self.entregado, ', Asignado:', self.asignado)
 
 
-class Conductor(Trabajador):
-    def __init__(self, dni: str, edad: int, nombre: str, direccion: str, disponible: bool, salario: float, licencia: str):
-        Trabajador.__init__(self, dni, edad, nombre,
-                            direccion, disponible, salario)
-        self.licencia = licencia
+class Pedido:
 
-    def get_conductor(self) -> str:
-        return f"Conductor: {self.nombre}, Licencia: {self.licencia}, Disponible: {self.disponible}, Salario: {self.salario}, Edad: {self.edad}, DNI: {self.dni}, Dirección: {self.direccion}"
+    def __init__(self, paquetes, canalEntrada, modalidadEnvio):
+        self.id = None
+        self.canalEntrada = canalEntrada
+        self.modalidadEnvio = modalidadEnvio
+        self.paquetes = paquetes
 
+    def calcularCoste(self):
+        coste = 0
+        for paquete in self.paquetes:
+            if paquete.destino == ListaDestinos.LORCA:
+                coste += 8
+            elif paquete.destino == ListaDestinos.CARTAGENA:
+                coste += 7
+            elif paquete.destino == ListaDestinos.MURCIA:
+                coste += 3
+            elif paquete.destino == ListaDestinos.YECLA:
+                coste += 12
+            elif paquete.destino == ListaDestinos.JUMILLA:
+                coste += 10
 
-# ayudante aún tiene que añadir cosas de herencia de Viaje y ParteViaje
-class Ayudante(Trabajador):
-    def __init__(self, dni: str, edad: int, nombre: str, direccion: str, disponible: bool, salario: float, experiencia_laboral: int):
-        Trabajador.__init__(self, dni, edad, nombre,
-                            direccion, disponible, salario)
-        self.experiencia_laboral = experiencia_laboral
+        # Coste adicional si el envío es a domicilio.
+        if self.modalidadEnvio == ModalidadEnvio.DOMICILIO:
+            coste += 5
 
-    def get_ayudante(self) -> str:
-        return f"Ayudante: {self.nombre}, Experiencia laboral: {self.experiencia_laboral}, Disponible: {self.disponible}, Salario: {self.salario}, Edad: {self.edad}, DNI: {self.dni}, Dirección: {self.direccion}"
+        return coste
+
+    def devolverDatos(self):
+        print('Pedido: ', self.id, ', Canal de Entrada: ',
+              self.canalEntrada, ', Modalidad de Envio: ', self.modalidadEnvio)
+
+        for paquete in self.paquetes:
+            paquete.devolverDatos()
 
 
 class Cliente(Persona):
-    def __init__(self, dni: str, edad: int, nombre: str, direccion: str):
-        Persona.__init__(self, dni, edad, nombre, direccion)
 
-    def get_cliente(self):
-        return f"Cliente: {self.nombre}, Edad: {self.edad}, DNI: {self.dni}, Dirección: {self.direccion}"
+    def __init__(self, nombre, direccion, dni):
+        Persona.__init__(self, nombre, direccion, dni)
+        self.pedidosmensuales = []
+        self.cesta = []
 
+    def devolverDatos(self):
+        print('Cliente:', self.nombre, ', Direccion:', self.direccion,
+              ', DNI:', self.dni, ', Pedidos mensuales:', self.pedidosmensuales, ', Cesta activa:', self.cesta)
 
-class Paquete(metaclass=ABCMeta):
-    def __init__(self, paquete_id: str, peso: float, destino: str):
-        self.paquete_id = paquete_id
-        self.peso = peso
-        self.destino = destino
+    def listadoPaquetes(self):
+        print('LISTADO PAQUETES')
+        for paquete in self.cesta:
+            print('Paquete:', paquete.id, ', Peso:',
+                  paquete.peso, ', Destino:', paquete.destino)
 
+    def realizarPedido(self, canal, envio, Empresa):
+        pedido = Pedido(self.cesta, canal, envio)
+        self.pedidosmensuales.append(pedido)
+        self.cesta = []
+        Empresa.capturarPedido(pedido)
 
-class Pedido(Paquete):
-    def __init__(self, pedido_id: str, paquetes: List[Paquete], canal_entrada: CanalEntrada, modalidad_envio: ModalidadEnvio):
-        Paquete.__init__(self, paquete_id, peso, destino)
-        self.pedido = pedido
-        self.cliente = cliente
-        self.paquetes = paquetes
-        self.canal_entrada = canal_entrada
-        self.modalidad_envio = modalidad_envio
-        self.conductor = None
+    def agregarPaquete(self, peso, destino):
+        paquete = Paquete(peso, destino)
+        paquete.id = len(self.cesta)
+        self.cesta.append(paquete)
 
-    def calcular_coste(self) -> float:
-        coste = sum(p.peso for p in self.paquetes)
-        if self.modalidad_envio == ModalidadEnvio.DOMICILIO:
-            coste += 10
-        return coste
+    def quitarPaquete(self, id):
+        for paquete in self.cesta:
+            if paquete.id == id:
+                self.cesta.remove(paquete)
 
-    def asignar_trabajador(self, trabajadores: List[Trabajador]):
-        for trabajador in trabajadores:
-            if isinstance(trabajador, Conductor) and trabajador.disponible:
-                self.conductor = trabajador
-                trabajador.cambiar_disponibilidad(False)
-                print(
-                    f"Conductor {trabajador.nombre} asignado al pedido {self.pedido_id}")
-                return
-
-        raise Exception("No hay conductores disponibles para este pedido.")
-
-    def asignar_vehiculo(self, vehiculo: Vehiculo):
-        if self.conductor is None:
-            raise Exception("No se puede asignar vehículo sin conductor.")
-        for paquete in self.paquetes:
-            vehiculo.asignar_paquete(paquete)
+    def getPedidosMensuales(self):
+        facturacion = 0
+        for pedido in self.pedidosmensuales:
+            facturacion += pedido.calcularCoste()
+        self.pedidosmensuales = []
+        return facturacion
 
 
-class Vehiculo(metaclass=ABCMeta):
-    def __init__(self, matricula: str, capacidad: float, paquetes: list[Paquete], disponible: bool):
-        self.matricula = matricula
-        self.capacidad = capacidad
-        self.paquetes = paquetes
-        self.disponible = disponible
+class Trabajador(Persona, metaclass=ABCMeta):
+
+    def __init__(self, nombre, direccion, dni, salario):
+        Persona.__init__(self, nombre, direccion, dni)
+        self.salario = salario
+        self.disponible = True
+
+    @abstractmethod
+    def devolverDatos(self):
+        pass
+
+    def cambiarDisponibilidad(self):
+        if self.disponible:
+            self.disponible = False
+        else:
+            self.disponible = True
+
+
+class Conductor(Trabajador):
+
+    def __init__(self, nombre, direccion, dni, salario):
+        Trabajador.__init__(self, nombre, direccion, dni, salario)
+
+    def devolverDatos(self):
+        print('Conductor:', self.nombre, ', Direccion:', self.direccion,
+              ', DNI:', self.dni, ', Salario:', self.salario, ', Disponible:', self.disponible)
+
+
+class Ayudante(Trabajador):
+
+    def __init__(self, nombre, direccion, dni, salario):
+        Trabajador.__init__(self, nombre, direccion, dni, salario)
+
+    def devolverDatos(self):
+        print('Ayudante:', self.nombre, ', Direccion:', self.direccion,
+              ', DNI:', self.dni, ', Salario:', self.salario, ', Disponible:', self.disponible)
+
+
+class Ambos(Ayudante, Conductor, Trabajador):  # duda
+    def __init__(self, nombre, direccion, dni, salario):
+        Ayudante.__init__(self, nombre, direccion, dni, salario)
+        Conductor.__init__(self, nombre, direccion, dni, salario)
+
+    def devolverDatos(self):
+        print('Ambos:', self.nombre, ', Direccion:', self.direccion,
+              ', DNI:', self.dni, ', salario:', self.salario, ', Disponible:', self.disponible)
